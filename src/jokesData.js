40491b2e5abe3371570f6bc44bb4932d7766db7e
Jovan Ssebaggala <jssebaggala@outlook.com>
2022-02-28 17:02:11 +0300

    Initial commit

---
+++ src/jokesData.js
@@ -0,0 +1,21 @@
+export default[
+    {
+        setup:'Coming up with jokes is not that easy', 
+        punchline: 'We shall get there'
+    },
+    {
+        setup: 'MC Kapale is actually funny',
+        punchline:'It is just haters that do not feel him'
+    },
+    {
+        setup: 'Testing 123  waguan',
+        punchline: 'Just for laughs'
+    },
+    {
+        setup: 'Short people are good people',
+        punchline: 'They are down to earth'
+    },
+    {
+        punchline:"We are going higher, that's evident, got the lion of Judah magnificent"
+    }
+]
